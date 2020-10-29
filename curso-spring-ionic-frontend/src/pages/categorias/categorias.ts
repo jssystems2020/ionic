fdaa6ed8ba596b7c698c42d7297e0a185cfaac56
jssystems2020 <jssystems2021@gmail.com>
2020-10-29 08:42:55 -0300

    Listando Categoria

--- curso-spring-ionic-frontend/src/pages/categorias/categorias.ts
+++ curso-spring-ionic-frontend/src/pages/categorias/categorias.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CategoriaService } from '../../services/categoria.service';
 
 /**
  * Generated class for the CategoriasPage page.
@@ -15,11 +16,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+     public navParams: NavParams,
+     public categoriaService: CategoriaService) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriasPage');
+  //  console.log('ionViewDidLoad CategoriasPage');
+  this.categoriaService.findAll()
+  .subscribe(Response => {
+    console.log(Response);
+  },
+    error => {
+      console.log(error);
+    });
   }
 
 }
